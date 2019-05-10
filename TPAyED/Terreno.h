656545c9a14b2a0afb8eef1c5cf12b19e2cad5b0
PabloDL <pablo.delafore@hotmail.com>
2019-05-09 21:31:39 -0300

    agregado terreno

---
+++ TPAyED/Terreno.h
@@ -0,0 +1,54 @@
+#ifndef TERRENO_H__
+#define TERRENO_H__
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct Terreno{
+    Locomotora locomotora;
+    Lista minas;
+    Lista estaciones;
+    Lista moneda;
+    Lista bandidos;
+}Terreno;
+
+
+/************GETTERS AND SETTERS*********************/
+
+//getters y setters
+void setLocoomotora(Vagon& vagon,int pesoMaximo);
+int getPesoMaximo(Vagon& vagon);
+
+
+/***********************PRIMITIVAS*********************/
+//pre:
+//post: se inicializan los parametros de Terreno
+void crearTerreno(Terreno& terreno);
+
+//pre:Tiene que existir el terreno
+//post:Se elimina el terreno en cascada con los atributos internos
+//se usa al cerrar el juego
+void eliminarTerreno(Terreno& terreno)
+
+//pre:Terreno tiene que estar creado e inicializado
+//post: se posiciona la locomotora en el terreno
+void aparecerLocomotora(Terreno& terreno);
+
+//pre:Terreno tiene que estar creado e inicializado
+//post: se posiciona una mina en el terreno
+void aparecerMina(Terreno& terreno);
+
+//pre:Terreno tiene que estar creado e inicializado
+//post:se posiciona una estacion en el terreno
+void aparecerEstacion(Terreno& terreno);
+
+//pre:Terreno tiene que estar creado e inicializado
+//post:se posiciona una moneda checkeando si es posible en el terreno
+void aparecerMoneda(Terreno& terreno);
+
+//pre:Terreno tiene que estar creado e inicializado
+//post: se posiciona una moneda checkeando si es posible en el terreno
+void aparecerBandido(Terreno& terreno);
+
+#endif // TERRENO_H__
