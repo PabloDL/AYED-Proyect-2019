9ba12d2a7765134a8ff811bed1612e130161d2c0
rociomtrinidad <50055634+rociomtrinidad@users.noreply.github.com>
2019-05-27 23:20:17 -0300

    Update Cajas.h

--- TPAyED/Cajas.h
+++ TPAyED/Cajas.h
@@ -6,31 +6,20 @@
 
 using namespace std;
 
-
-
 /*
-
     Axiomas
 
     No puede contener mas de 5 item.
-
 */
 
-
-
 /* Tipo de estructura de Cajas */
-
-
 
 typedef struct Cajas{
    int cantitem;
    string codItem;
 } Cajas;
 
-
-
 /************************************************/
-
 
 /*PRE: Que la caja existe
 
@@ -40,34 +29,13 @@ Post: Devuelve cantidad de item
 
 int getItem(Cajas &cajas);
 
-
-/*PRE:
-
-Post: inicializa parametros de la caja
-
-*/
-
-void crearCaja(Cajas &cajas);
-
-
 /*PRE: Que la caja existe
 
 Post: Devuelve cantidad de item
 
 */
 
-void modificarCantItem(Cajas &cajas);
-
-
-/*PRE: Estacion creada
-
-Post: libera recursos solicitados por estacion
-
-*/
-
-void eliminarCaja(Cajas &cajas);
-
-
+void setCantItem(Cajas &cajas);
 
 
 #endif // __PARAMETROS_H__
